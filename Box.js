2b5387b0938d316516968a4339bd2e65c15d9604
Haaziq107 <66906567+Haaziq107@users.noreply.github.com>
2020-08-11 17:44:14 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Block extends BaseClass {
+  constructor(x, y) {
+      super(x,y,50,50);
+      this.visiblity=255;
+      World.add(world, this.body);
+  }
+
+  display(){
+    var pos =this.body.position;
+      console.log(this.body.speed);
+      if (this.body.speed<5){
+        super.display();
+      }else{
+        World.remove(world,this.body)
+        push();
+        this.visiblity=this.visiblity-10;
+        tint(255,this.visiblity);
+        image(this.image,this.body.position.x,this.body.position.y);
+        pop();
+      }
+  }
+};
